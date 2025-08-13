366ff0a80120634e0cdac40c5e83ba932714dd33
alexandrenovo2001 <60365638+alexandrenovo2001@users.noreply.github.com>
2025-08-13 15:33:23 +0100

    Improvement on internet profile

--- internet-profile.kt
+++ internet-profile.kt
@@ -12,11 +12,22 @@ class Person(val name: String, val age: Int, val hobby: String?, val referrer: P
        // Fill in code 
        println("Name: $name")
        println("Age: $age")
+        
+       if(hobby != null) {
+           print("Likes to $hobby. ")
+       }
        
-        if (referrer == null) {
-            println("Likes ${hobby}. Doesn't have a referrer.")
-        } else {
-            println("Likes ${hobby}. Has a referrer named ${referrer!!.name}, who likes to ${referrer?.hobby}.")
-        }
+       if(referrer != null) {
+           print("Has a referrer named ${referrer.name}")
+            if(referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}.")
+            } else {
+                print(".")
+            }
+       } else {
+           print("Doesn't have a referrer.")
+       }
+       
+       print("\n\n")
     }
 }
